1ee1180faeddf15f415e6eb607c678f59039aba8
eugen <evgeniy.konyshev2007@gmail.com>
2022-06-08 13:51:45 +0200

    Minor changes to create a base fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world!')
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/', (_, reply) => {
+  reply.send('This server is running!')
+})
+
+fastify.listen(3000, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
